793671299b1160ca496e24b8f7a10e95d1503f5c
Wakag <g22g8069@campus.ru.ac.za>
2024-05-19 14:56:03 +0200

    Add sumElements method to SimpleFunctions class

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    
+public static int sumElements(List<Integer> a, List<Integer> b) {
+    int total = 0; 
+    for ( int index : b){
+        if (index >= 0 && index < a.size()){
+            total += a.get(index);
+        }
+    }
+    return total; 
+}
+
+
 }
