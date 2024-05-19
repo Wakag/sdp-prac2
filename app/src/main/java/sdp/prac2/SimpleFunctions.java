dc8bd86f8394b82d009e1b24219360583e293802
OtherSiya <g21h8394@campus.ru.ac.za>
2024-05-19 15:21:28 +0200

    Added task 4 to SimpleFunctions

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,22 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+
+        public addList(int [] a, int [] b){
+            if (a.length != b.length){
+                return NULL;
+            }
+            else{
+                int [] answer = new int[a.length];
+
+                for(int i = 0; i < a.length; i++){
+                    for(int j = b.length - 1; j >= 0; j--){
+                        answer[i] = a[i] + b[j];
+                    }
+                    return answer;
+                }
+            }
+        }
+    }
 }
