0d9fe921d6679c076ee61df01f9ed5220845b719
SiyaHopu <113137595+SiyaHopu@users.noreply.github.com>
2024-05-20 23:19:04 +0200

    Updated the task4 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -5,20 +5,18 @@ import java.util.*;
 public class SimpleFunctions {
     public SimpleFunctions() {
 
-        public addList(int [] a, int [] b){
+        public static int [] task4(int [] a, int [] b){
             if (a.length != b.length){
-                return NULL;
+                return null;
             }
             else{
                 int [] answer = new int[a.length];
 
                 for(int i = 0; i < a.length; i++){
-                    for(int j = b.length - 1; j >= 0; j--){
-                        answer[i] = a[i] + b[j];
+                    answer[i] = a[i] + b[a.length - 1- i];
                     }
                     return answer;
                 }
-            }
         }
     }
 }
