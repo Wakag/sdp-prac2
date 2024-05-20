d9b4d1d34652cef62de80d4764930d8370a563f6
SiyaHopu <113137595+SiyaHopu@users.noreply.github.com>
2024-05-21 00:08:37 +0200

    updated task4

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -16,4 +16,17 @@ public class SimpleFunctions {
         }
         return roundedNumbers;
     }
+
+    public static int [] task4(int [] a, int [] b){
+        if (a.length != b.length){
+            return null;
+        } else {
+            int [] answer = new int[a.length];
+
+            for(int i = 0; i < a.length; i++){
+                answer[i] = a[i] + b[a.length - 1- i];
+            }
+                return answer;
+            }
+        }
 }
