7c4f4437c232961992ab63d31dcdbc54e7f41535
kwenza05 <g20h4776@campus.ru.ac.za>
2024-05-20 19:10:41 +0200

    task 6 added and fixed

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    public static List<Integer> Task6(List<Integer> numbers) {
+        List<Integer> roundedNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % 100 == 0) {
+                roundedNumbers.add(number);
+            } else {
+                int roundedNumber = ((number / 100) + 1) * 100;
+                roundedNumbers.add(roundedNumber);
+            }
+        }
+        return roundedNumbers;
+    }
 }
